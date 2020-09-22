2469a1bf1a253ec11b9a7aced7ba4dcb1eb26b85
ir2020 <indrek.runkaru@khk.ee>
2020-09-22 07:01:05 +0300

    Ülesanne 3

--- src/Controller/BookingIndex.php
+++ src/Controller/BookingIndex.php
@@ -6,6 +6,14 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TimeType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType ;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
 class BookingIndex extends AbstractController
 {
     /**
@@ -13,7 +21,47 @@ class BookingIndex extends AbstractController
      */
     public function create_booking(): Response
     {
-        return $this->render('bookings/create_booking.html.twig');
+        $form = $this->createFormBuilder()
+            ->add('First-name', TextType::class, [
+                'required' => true
+            ])
+            ->add('Last-name', TextType::class, [
+                'required' => true
+            ])
+            ->add('Phone-number', TextType::class, [
+                'required' => true
+            ])
+            ->add('Email', TextType::class, [
+                'required' => false
+            ])
+            ->add('Birthday', DateType::class, [
+                'required' => true,
+            ])
+            ->add('Start-date', DateType::class, [
+                'required' => true,
+            ])
+            ->add('nrOfPeople', IntegerType::class, [
+                'required' => true,
+            ])
+            ->add('End-date', DateType::class, [
+                'required' => true,
+            ])
+            ->add('How-to-pay', ChoiceType::class, [
+                'choices' => [
+                    'money',
+                    'card',
+                    'check',
+                    'chickens'
+                ]
+            ])
+            ->add('additional-information', TextareaType::class, [
+            ])
+            ->add('save', SubmitType::class, ['label' => 'Create booking'])
+            ->getForm();
+
+        return $this->render('bookings/create_booking.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 
     /**
